bf775208244d150dfc44a3e57a23b79ab0c340a1
Giuseppe Eletto <peppe.eletto@gmail.com>
2019-11-13 17:11:24 +0100

    WIP

--- android_client/app/src/main/java/com/github/peppe998e/unitoreps/modules/apiservice/ApiServiceImpl.java
+++ android_client/app/src/main/java/com/github/peppe998e/unitoreps/modules/apiservice/ApiServiceImpl.java
@@ -1,8 +1,11 @@
 package com.github.peppe998e.unitoreps.modules.apiservice;
 
 public class ApiServiceImpl implements ApiService {
+    private final String apiUrl;
 
     public ApiServiceImpl(String ApiUrl) {
-
+        this.apiUrl = ApiUrl;
     }
+
+
 }
