c7f16569bb8be71808c02c7948e402e593a44522
Giuseppe Eletto <peppe.eletto@gmail.com>
2020-01-15 16:55:16 +0100

    WIP

--- client/app/src/main/java/it/unito/ium/myreps/controllers/LessonController.java
+++ client/app/src/main/java/it/unito/ium/myreps/controllers/LessonController.java
@@ -2,25 +2,36 @@ package it.unito.ium.myreps.controllers;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import it.unito.ium.myreps.R;
 import it.unito.ium.myreps.components.RecyclerViewRow;
 
 public final class LessonController extends BaseController {
     private RecyclerViewRow lesson;
 
+    @BindView(R.id.lesson_header_subject)
+    TextView headerSubject;
+    @BindView(R.id.lesson_header_prof)
+    TextView headerProfessor;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lesson_view);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ButterKnife.bind(this);
 
         lesson = (RecyclerViewRow) getIntent().getSerializableExtra("lesson");
-        Toast.makeText(this, lesson.getSubject(), Toast.LENGTH_SHORT).show();
+
+        headerSubject.setText(lesson.getSubject());
+        headerProfessor.setText(lesson.getProfessor());
     }
 
     @Override
